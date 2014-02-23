7f12231088e019e08bf707bb0a30493f1ac5a34b
unknown <zeta@.(none)>
2014-02-23 09:58:19 +0700

    Test Commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -97,6 +97,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
+		<p> RABBITERS </p>
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
